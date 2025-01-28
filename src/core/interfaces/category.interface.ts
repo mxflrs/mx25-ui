e2fae476af4c0d5ac189c0128f62e5f24bb16180
mxflrs <max@maxflores.dev>
2025-01-28 16:11:16 -0600

    wip: implement technologies

--- src/core/interfaces/category.interface.ts
+++ src/core/interfaces/category.interface.ts
@@ -1,7 +1,7 @@
 import type { ITechnology } from "./technology.interface";
 
 export interface ICategory {
-    name: string;
+    category: string;
     icon: string;
     focus: {
         title: string;
