454730c4bbbeafaddaf557c318ff608a7aa4c8bc
mxflrs <max@maxflores.dev>
2025-01-30 01:31:57 -0600

    wip: trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import htmx from 'astro-htmx';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), htmx()],
   vite: {
